d1595a758b21aa0d4ab3bd732d716b4eb9c6876d
EliasSchramm <privat@eps-dev.de>
2020-08-08 12:32:21 +0200

    Update

--- src/main/java/de/epsdev/plugins/MMO/events/e_BlockDestroy.java
+++ src/main/java/de/epsdev/plugins/MMO/events/e_BlockDestroy.java
@@ -5,17 +5,16 @@ import de.epsdev.plugins.MMO.data.output.Out;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
 
 public class e_BlockDestroy implements Listener {
     @EventHandler
-    void onBlockDestroyed(BlockDamageEvent e){
-        if(!e.getPlayer().getGameMode().equals(GameMode.CREATIVE)){
+    void onBlockDestroyed(BlockBreakEvent e){
+        if(!e.getPlayer().getGameMode().equals(GameMode.CREATIVE)
+                || !DataManager.onlineUsers.get(e.getPlayer().getUniqueId().toString()).rank.canBuild){
             e.setCancelled(true);
         }
-        if(!DataManager.onlineUsers.get(e.getPlayer().getUniqueId().toString()).rank.canBuild){
-            e.setCancelled(true);
-            Out.printToBroadcast("lol");
-        }
+
     }
 }
--- src/main/java/de/epsdev/plugins/MMO/events/e_BlockPlace.java
+++ src/main/java/de/epsdev/plugins/MMO/events/e_BlockPlace.java
@@ -1,6 +1,7 @@
 package de.epsdev.plugins.MMO.events;
 
 import de.epsdev.plugins.MMO.data.DataManager;
+import de.epsdev.plugins.MMO.data.output.Out;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
