eabc66cdac71e9a516e996f3a95d6f424ad56e23
EliasSchramm <privat@eps-dev.de>
2020-08-18 22:32:06 +0200

    Update

---
+++ src/main/java/de/epsdev/plugins/MMO/commands/c_createhouse.java
@@ -0,0 +1,12 @@
+package de.epsdev.plugins.MMO.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class c_createhouse implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
