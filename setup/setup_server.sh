5aa90331b0e2582ee0181fe4166928f10b6bffd4
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2021-02-01 13:06:11 +0100

    Create setup_server.sh

---
+++ setup/setup_server.sh
@@ -0,0 +1,18 @@
+#updatating the system
+
+apt-get update
+apt-get upgrade -y
+
+#installing dependencies
+
+apt-get install openjdk-8-jdk-headless screen git
+
+#installing the spigot server
+
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -Xms512M -Xmx512M -jar BuildTools.jar --rev 1.12.2
+
+echo "eula=true" > eula.txt
+echo "java -Xms1G -Xmx1G -jar spigot-1.12.2.jar -nogui" > start.sh
+
+chmod +x ./*
