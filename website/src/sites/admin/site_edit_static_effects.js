da9ffb525cb290ae6e287343eb86a2bb5328b0a2
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2021-02-25 21:08:08 +0100

    Small aditions to the static_effect UI
    I can't do to much rn cause of school work.

--- website/src/sites/admin/site_edit_static_effects.js
+++ website/src/sites/admin/site_edit_static_effects.js
@@ -6,6 +6,52 @@ import Skin_Obj from '../general/Skin_Obj'
 const coockie = require('../../tools/coockies');
 const api = require('../../tools/api/api');
 
+class Effect_type_select extends React.Component{
+    render(){
+
+        let options = [];
+
+        Object.entries(particleTypes).forEach(([key, value]) => {
+            if(key == this.props.cur){
+                options.push(<option value={key} selected="selected">{value}</option>);
+            }else{
+                options.push(<option value={key}>{value}</option>);
+            }
+            
+        });         
+
+        return(
+            <select name="type_option" id="type_option" e_id={0} onChange={this.props.handler}>
+                {                    
+                    options                
+                }
+            </select>
+        );
+    }
+}
+
+class Effect_type_form extends React.Component{
+    render(){
+        switch(this.props.data[0]){
+            case "single":
+                return(
+                    <input e_id={0} type='text' value={"this.state.data[0]"} onChange={this.handleChange}/>
+                );
+                break;
+           
+            case "circle":
+                return(
+                    <h3>2</h3>
+                );
+                break;
+        }
+        return(
+            <h3>ERROR: Type Not Found</h3>
+        );
+    }
+}
+
+
 class Effect_edit extends React.Component {
 
     constructor(probs) {
@@ -59,42 +105,20 @@ class Effect_edit extends React.Component {
 
         this.forceUpdate()
     }
-    
 
-    render() {
-        
-        let options = [];
-
-        Object.entries(particleTypes).forEach(([key, value]) => {
-            options.push(<option value={key}>{value}</option>);
-        });
-         
-
+    render() {         
         return (
             <div class="Site_Edit_Effects"> 
-                <h2>Currently selected: {this.state.npc_name} [ID:{this.state.npc_id}]</h2>
-                
+                <h2>Currently selected: {particleTypes[this.state.data[0]]} [ID:{this.state.effect_id}]</h2>                
                 <form>
-
-                    <select name="pets" id="pet-select">
-                        {                    
-                            options                    
-                        }
-                    </select>
-
-
-              
+                    <Effect_type_select cur={this.state.data[0]} handler={this.handleChange}/>                
                     <br/>
-
-                    <label><h3>Name</h3></label>                
-                    <input e_id={0} type='text' value={this.state.data[0]} onChange={this.handleChange}/>
-
+                    <Effect_type_form data={this.state.data} handler={this.handleChange} />
                     <br/>
                     <input type="button" value="Submit" onClick={this.onSubmit}/>
                 </form>
-
             </div>
-        );
+        );        
     }
 }
 
