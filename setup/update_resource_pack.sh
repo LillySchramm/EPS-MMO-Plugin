4dec421dc9b554694dea2d0b13c251a0144066c2
EPS-DEV <49179221+EliasSchramm@users.noreply.github.com>
2021-03-15 19:56:40 +0100

    Fixed typo

--- setup/update_resource_pack.sh
+++ setup/update_resource_pack.sh
@@ -1,7 +1,8 @@
-base="resource-pack=http\:\/\/0.0.0.0\:10100\/resourcepack\/"
+base='resource-pack=http:\/\/0.0.0.0:10100\/resourcepack\/download\/'
 
 ver=`curl 0.0.0.0:10100/resourcepack/version | awk '{ print substr ($0, 8 ) }' | awk '{ print substr( $0, 1, length($0)-1 ) }'`
 
 regex='30s/.*/'$base$ver'/'
+echo $regex
 
 sed -i $regex server.properties
